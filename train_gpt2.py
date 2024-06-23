9dd2bef663deecbe4abaebb6238b8236d47c19ac
masonwang025 <masonwang025@gmail.com>
2024-06-23 14:33:23 -0700

    add mlp

--- train_gpt2.py
+++ train_gpt2.py
@@ -12,10 +12,43 @@ class GPTConfig:
     n_heads: int = 12
 
 
+class CausalSelfAttention(nn.Module):
+    # think of self attention as where the tokens communicate with each other
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+
+
+class MLP(nn.Module):
+    # think of MLP as where the model thinks about the gathered information from attn
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        # one linear layer to size 4 * n_embd
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        # gelu with tanh approximation
+        self.gelu = nn.GELU(approximate="tanh")
+        # one linear layer back to size n_embd
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
 class Block(nn.Module):
     def __init__(self, config: GPTConfig):
         super().__init__()
-        self.config = config
+        # layer norm -> self attention -> layer norm -> mlp
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
 
 
 class GPT(nn.Module):
