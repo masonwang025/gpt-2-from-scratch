7b70ef129629ecb8334482ef066165b976ca1827
masonwang025 <masonwang025@gmail.com>
2024-06-30 17:12:31 -0400

    eliezer shards

--- data/eliezer.py
+++ data/eliezer.py
@@ -10,7 +10,7 @@ import json
 
 # Set up directories and constants
 local_dir = "yudkowsky_lesswrong"
-shard_size = int(1e8)  # 100M tokens per shard
+shard_size = int(1e6)  # 1M tokens per shard
 
 # Create the local directory if it doesn't exist yet
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
@@ -113,9 +113,6 @@ else:
 
 print(str(len(documents)) + " documents")
 print("Avg len for document:", sum(len(doc) for doc in documents) / len(documents))
-import sys
-
-sys.exit()
 
 # Initialize the tokenizer
 enc = tiktoken.get_encoding("gpt2")
@@ -138,42 +135,45 @@ def write_datafile(filename, tokens_np):
     np.save(filename, tokens_np)
 
 
-# Tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
-nprocs = max(1, os.cpu_count() // 2)
-with mp.Pool(nprocs) as pool:
-    shard_index = 0
-    # Preallocate buffer to hold current shard
-    all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
-    token_count = 0
-    progress_bar = None
-    for tokens in pool.imap(tokenize, documents, chunksize=16):
+if __name__ == "__main__":
+    # Tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
+    nprocs = max(1, os.cpu_count() // 2)
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        # Preallocate buffer to hold current shard
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, documents, chunksize=16):
 
-        # Is there enough space in the current shard for the new tokens?
-        if token_count + len(tokens) < shard_size:
-            # Simply append tokens to current shard
-            all_tokens_np[token_count : token_count + len(tokens)] = tokens
-            token_count += len(tokens)
-            # Update progress bar
-            if progress_bar is None:
-                progress_bar = tqdm(
-                    total=shard_size, unit="tokens", desc=f"Shard {shard_index}"
-                )
-            progress_bar.update(len(tokens))
-        else:
-            # Write the current shard and start a new one
+            # Is there enough space in the current shard for the new tokens?
+            if token_count + len(tokens) < shard_size:
+                # Simply append tokens to current shard
+                all_tokens_np[token_count : token_count + len(tokens)] = tokens
+                token_count += len(tokens)
+                # Update progress bar
+                if progress_bar is None:
+                    progress_bar = tqdm(
+                        total=shard_size, unit="tokens", desc=f"Shard {shard_index}"
+                    )
+                progress_bar.update(len(tokens))
+            else:
+                # Write the current shard and start a new one
+                filename = os.path.join(DATA_CACHE_DIR, f"lesswrong_{shard_index:06d}")
+                # Split the document into whatever fits in this shard; the remainder goes to next one
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count : token_count + remainder] = tokens[
+                    :remainder
+                ]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                # Populate the next shard with the leftovers of the current doc
+                all_tokens_np[0 : len(tokens) - remainder] = tokens[remainder:]
+                token_count = len(tokens) - remainder
+
+        # Write any remaining tokens as the last shard
+        if token_count != 0:
             filename = os.path.join(DATA_CACHE_DIR, f"lesswrong_{shard_index:06d}")
-            # Split the document into whatever fits in this shard; the remainder goes to next one
-            remainder = shard_size - token_count
-            progress_bar.update(remainder)
-            all_tokens_np[token_count : token_count + remainder] = tokens[:remainder]
-            write_datafile(filename, all_tokens_np)
-            shard_index += 1
-            progress_bar = None
-            # Populate the next shard with the leftovers of the current doc
-            all_tokens_np[0 : len(tokens) - remainder] = tokens[remainder:]
-            token_count = len(tokens) - remainder
-
-    # Write any remaining tokens as the last shard
-    if token_count != 0:
-        filename = os.path.join(DATA_CACHE_DIR, f"lesswrong_{shard_index:06d}")
-        write_datafile(filename, all_tokens_np[:token_count])
+            write_datafile(filename, all_tokens_np[:token_count])
